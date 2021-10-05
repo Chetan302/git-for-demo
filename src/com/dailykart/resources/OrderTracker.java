724eedf5222c5e9f3d055b0ca19df62d11984261
chetan302 <chaudharichetan943@gmail.com>
2021-10-05 16:36:17 +0530

    added order tracker object

--- src/com/dailykart/resources/OrderTracker.java
+++ src/com/dailykart/resources/OrderTracker.java
@@ -5,6 +5,7 @@ import com.dailykart.bo.OrderDetails;
 public class OrderTracker {
 
 	public OrderDetails getOrderDetails(long airwayBillNo) {
+		OrderDetails od = new OrderDetails();
 		return new OrderDetails();
 	}
 }
